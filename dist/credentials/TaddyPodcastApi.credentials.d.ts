772dc687a053c3683e98b7d6386962a938819aad
Logan Shimmer <loganherr@gmail.com>
2025-09-24 09:09:15 -0500

    Add dist and node_modules to gitignore

--- dist/credentials/TaddyPodcastApi.credentials.d.ts
+++
@@ -1,8 +0,0 @@
-import { ICredentialType, INodeProperties, ICredentialTestRequest } from 'n8n-workflow';
-export declare class TaddyPodcastApi implements ICredentialType {
-    name: string;
-    displayName: string;
-    documentationUrl: string;
-    properties: INodeProperties[];
-    test: ICredentialTestRequest;
-}
--- dist/nodes/TaddyPodcast/TaddyPodcast.node.d.ts
+++
@@ -1,5 +0,0 @@
-import { INodeType, INodeTypeDescription, IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
-export declare class TaddyPodcast implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
